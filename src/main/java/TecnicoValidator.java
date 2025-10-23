761085348845284a033726ffcd033e4648ba71a6
Adryel <adryxd003@gmail.com>
2025-10-23 18:30:41 -0300

    Feat: Adiciona TecnicoValidator com verificações de Integridade e Negócio

---
+++ src/main/java/TecnicoValidator.java
@@ -0,0 +1,20 @@
+public class TecnicoValidator {
+
+    // -- Métodos de Regras de Negócio -- //
+    public void verificaRegrasFormacao(String formacao) {
+        if(formacao == null) {
+            throw new IllegalStateException("ERRO! A FORMAÇÃO NÃO PODE SER NULA");
+        }
+
+        if(formacao.length() < 6) {
+            throw new IllegalStateException("ERRO! A FORMAÇÃO DEVE CONTER MAIS DE 5 DÍGITOS");
+        }
+    }
+
+    // -- Métodos de Integridade -- //
+    public static void verificaIntegridadeFormacao(String formacao) {
+        if(formacao.isBlank()) {
+            throw new IllegalArgumentException("ERRO! FORMAÇÃO NÃO PODE SER VAZIA");
+        }
+    }
+}
