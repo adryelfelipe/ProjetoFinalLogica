aa6c6ed7a2c13345d88bc7b05719abf3a72880e0
GUSTAVO NOGATH <gustavo_nogath@estudante.sesisenai.org.br>
2025-10-23 19:48:15 -0300

    Especialidade adicionada

---
+++ src/main/java/ProjetoBase/Especialidade.java
@@ -0,0 +1,7 @@
+package ProjetoBase;
+
+public class Especialidade {
+    public static int Curso_Tecnico = 1;
+    public static int Ingles_Avancado = 2;
+    public static int Experiencia_Fabril = 3;
+}
