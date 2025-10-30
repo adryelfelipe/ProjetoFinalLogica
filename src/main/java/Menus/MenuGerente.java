33c8a204173b0eadfecd423581fdb31090d2951e
Daniel <daniel_e_dapper@estudante.sesisenai.org.br>
2025-10-30 16:48:36 -0300

    Criação e Atualização de Classes

--- src/main/java/Menus/MenuGerente.java
+++ src/main/java/Menus/MenuGerente.java
@@ -1,12 +1,35 @@
 package Menus;
 
+import ProjetoBase.Ferramentas;
 import ProjetoBase.GerenteModel;
 
 public class MenuGerente
 {
     public static void menuInicial(GerenteModel gerenteModel)
     {
+        System.out.println("|=================================|");
+        System.out.println("|=========  MENU GERENTE  ========|");
+        System.out.println("|=================================|");
 
+        System.out.println("|  1 - Cadastro Usuários          |");
+        System.out.println("|  2 - Visualizar Relatórios      |");
+        System.out.println("|=================================|");
+        System.out.print("|  Escolha: ");
 
+        try
+        {
+            int opcaoGerente = Ferramentas.lInteiro();
+
+            switch(opcaoGerente)
+            {
+                case 1 -> MenuGerenteCriarUsuario.criarUsuarios(gerenteModel);
+                case 2 -> MenuGerenteVisualizarRelatorios.visualizarRelatorios();
+                default -> Ferramentas.menuDefault();
+            }
+        }
+        catch(IllegalArgumentException | IllegalStateException e)
+        {
+            Ferramentas.menuDefault();
+        }
     }
 }
