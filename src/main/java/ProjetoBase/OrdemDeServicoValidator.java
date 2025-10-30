04156d4c9e0f886f9ce0cd334ef8d5c7e7a5e9e1
GUSTAVO NOGATH <gustavo_nogath@estudante.sesisenai.org.br>
2025-10-30 20:08:08 -0300

    ATUALIZAÇÃO VALIDATOR ORDEM DE SERVIÇO

--- src/main/java/ProjetoBase/OrdemDeServicoValidator.java
+++ src/main/java/ProjetoBase/OrdemDeServicoValidator.java
@@ -3,32 +3,50 @@ package ProjetoBase;
 public class OrdemDeServicoValidator
 {
     // -- Verifica regras de negócio -- //
-    public static void verificaRegrasNomeFuncionario(String nome) {
-        if (nome == null) {
-            throw new IllegalStateException("ERRO! O NOME NÃO PODE SER NULO");
+    public static void verificaRegrasDescricao(String descricao) {
+        if (descricao == null) {
+            throw new IllegalStateException("ERRO! A DESCRIÇÃO NÃO PODE SER NULO");
         }
 
-        if (nome.length() < 2) {
-            throw new IllegalStateException("ERRO! O NOME DEVE CONTER MAIS DE 1 CARACTER");
+        if (descricao.length() < 5 || descricao.length() > 100) {
+            throw new IllegalStateException("ERRO! A DESCRIÇÃO DEVE CONTER MAIS DE 1 CARACTER E MENOS QUE 100");
         }
     }
 
-    public static void verificaRegrasStatus(int idStatus){
-        if(idStatus > 3) {
+    public static void verificaRegrasStatus(int Status){
+        if(Status > 3) {
             throw new IllegalArgumentException("ERRO! O ID_STATUS NÃO PODE SER MAIOR QUE 3");
         }
     }
 
-    // -- Verifica integridade de dados -- //
-    public static void verificaIntegridadeNomeFuncionario(String nome) {
-        if(nome.isBlank()) {
-            throw new IllegalArgumentException("ERRO! O NOME NÃO PODE SER VAZIO");
+    public static void verificaIntegridadeStatus(int Status) {
+        if(Status < 0) {
+            throw new IllegalArgumentException("ERRO! O ID_STATUS NÃO PODE SER MENOR QUE 0");
         }
     }
 
-    public static void verificaIntegridadeStatus(int idNivelAcesso){
-        if(idNivelAcesso < 0) {
-            throw new IllegalArgumentException("ERRO! O ID_STATUS NÃO PODE SER NEGATIVO");
+    // -- Verifica integridade de dados -- //
+    public static void verificaIntegridadeIdTecnico(long idTecnico) {
+        if(idTecnico < 0) {
+            throw new IllegalArgumentException("ERRO! O ID TÉCNICO NÃO PODE SER NEGATIVO");
+        }
+    }
+
+    public static void verificaIntegridadeIdOrdem_Servico(long idOrdem_Servico) {
+        if(idOrdem_Servico < 0) {
+            throw new IllegalArgumentException("ERRO! O ID DA ORDEM NÃO PODE SER NEGATIVO");
+        }
+    }
+
+    public static void verificaIntegridadeIdMaquina(long idMaquina) {
+        if(idMaquina < 0) {
+            throw new IllegalArgumentException("ERRO! O ID MAQUINA NÃO PODE SER NEGATIVO");
+        }
+    }
+
+    public void verificarValorDaOrdemDeServico(double valorDaOrdemDeServico) {
+        if (valorDaOrdemDeServico < 0.0) {
+            throw new IllegalArgumentException("O VALOR DA ORDEM DE SERVIÇO NAO PODE SER NEGATIVO");
         }
     }
 }
