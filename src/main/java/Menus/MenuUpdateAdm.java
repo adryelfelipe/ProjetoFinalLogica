88478e6f2136dfdeca50bb867ae3de42bd48f0df
Daniel <daniel_e_dapper@estudante.sesisenai.org.br>
2025-11-03 15:34:26 -0300

    CLasses Updates

---
+++ src/main/java/Menus/MenuUpdateAdm.java
@@ -0,0 +1,13 @@
+package Menus;
+
+public class MenuUpdateAdm
+{
+    public static void menuUpdateInicial()
+    {
+        System.out.println("|===============================|");
+        System.out.println("|======  MENU UPDATE ADM  ======|");
+        System.out.println("|===============================|\n");
+
+        System.out.println("|  1 - |");
+    }
+}
