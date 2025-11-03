e762a249bc3aca1539cf1b58bb4571ce01e9ea27
Daniel <daniel_e_dapper@estudante.sesisenai.org.br>
2025-11-03 16:54:56 -0300

    Classe Update Gerente

--- src/main/java/Views/MenuAdminUpdateGerente.java
+++ src/main/java/Views/MenuAdminUpdateGerente.java
@@ -4,21 +4,21 @@ import Models.AdminModel;
 import Models.GerenteModel;
 import ProjetoBase.GerenteService;
 import ProjetoBase.UsuarioService;
+import Repositories.GerenteDAO;
 import Repositories.UsuarioDAO;
 import Util.Ferramentas;
 
 import java.sql.SQLOutput;
 
-public class MenuAdminUpdateGerente
-{
-    private static final UsuarioService usuarioService = new UsuarioService();
-    private static final GerenteService gerenteService = new GerenteService();
-    private static final UsuarioDAO usuarioDAO = new UsuarioDAO();
+public class MenuAdminUpdateGerente {
 
-    public static void updateGerente(AdminModel adminModel)
-    {
+    private static final GerenteService gerenteService = new GerenteService();
+    private static final UsuarioService usuarioService = new UsuarioService();
+
+    public static void updateGerente(AdminModel adminModel) {
         boolean verifica = false;
         long idGerente;
+        int opcaoAdm = 0;
 
         try {
             idGerente = MenuEscolhaId.escolhaIdUpdate();
@@ -27,9 +27,9 @@ public class MenuAdminUpdateGerente
             return;
         }
 
-        GerenteModel gerente = ((GerenteModel) usuarioDAO.findById(idGerente));
+        GerenteModel gerente = ((GerenteModel) usuarioService.findById(idGerente));
 
-        while(!verifica)
+        while (!verifica)
         {
             System.out.println("|================================|");
             System.out.println("|======  ATUALIZAR GERENTE  =====|");
@@ -42,7 +42,46 @@ public class MenuAdminUpdateGerente
             System.out.println(" [1] - Nome                          |Departamento: " + gerente.getDepartamento());
             System.out.println(" [2] - CPF                           ");
             System.out.println(" [3] - Senha                         ");
-            System.out.println(" [9] - Departamento                  ------------------------");
+            System.out.println(" [4] - Departamento                  ");
+            System.out.println(" [5] - Retornar                      ------------------------");
+
+            try {
+                opcaoAdm = Ferramentas.lInteiro();
+
+                switch (opcaoAdm) {
+                    case 1 -> {
+                        String nome = MenuSetUsuario.MenuSetNome();
+                        usuarioService.updateNomeUsuario(adminModel, idGerente, nome);
+                        gerente.setNome(nome);
+                    }
+
+                    case 2 -> {
+                        String cpf = MenuSetUsuario.MenuSetCpf();
+                        usuarioService.updateCpfUsuario(adminModel, idGerente, cpf);
+                        gerente.setCpf(cpf);
+                    }
+
+                    case 3 -> {
+                        String senha = MenuSetUsuario.MenuSetSenha();
+                        usuarioService.updateSenhaUsuario(adminModel, idGerente, senha);
+                        gerente.setSenha(senha);
+                    }
+                    case 4 -> {
+                        int departamento = MenuSetGerente.menuSetDepartamento();
+                        gerenteService.updatedepartamento(adminModel, idGerente, departamento);
+                        gerente.setDepartamento(departamento);
+                    }
+                    case 5 ->
+                    {
+                        return;
+                    }
+                    default -> Ferramentas.menuDefault();
+                }
+            }
+            catch(IllegalArgumentException e)
+            {
+                Ferramentas.mensagemErro(e.getMessage());
+            }
         }
     }
 }
