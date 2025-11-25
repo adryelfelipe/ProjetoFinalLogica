e5d9299d2e375539d593b6ce331edf4365bbd421
DANIEL ERTHAL DAPPER <daniel_e_dapper@estudante.sesisenai.org.br>
2025-11-25 20:23:08 -0300

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,16 @@ import java.sql.SQLOutput;
 public class Main
 {
     public static void main(String[] args) throws SQLException {
-        System.out.println(Ferramentas.OCEAN_BLUE +"█████████████████████████████████████");
-        System.out.println("███  ");
-        System.out.println("███   ███   ███   ███████   █████████");
-        System.out.println("███   ███   ███   ███       ███   ███");
-        System.out.println("███   ███   ███   ███████   ███   ███");
-        System.out.println("███   ███   ███   ███       ███   ███");
-        System.out.println("███████████████   ███████   █████████");
-        System.out.println("                                  ███");
-        System.out.println("█████████████████████████████████████"+Ferramentas.RESET);
-        System.out.println("▀▄▀▄▀▄");
+        System.out.println(Ferramentas.OCEAN_BLUE +"         █████████████████████████████████████");
+        System.out.println("         ███  ");
+        System.out.println("         ███   ███   ███   ███████   █████████");
+        System.out.println("         ███   ███   ███   ███       ███   ███");
+        System.out.println("         ███   ███   ███   ███████   ███   ███");
+        System.out.println("         ███   ███   ███   ███       ███   ███");
+        System.out.println("         ███████████████   ███████   █████████");
+        System.out.println("                                           ███");
+        System.out.println("         █████████████████████████████████████"+Ferramentas.RESET);
+
 
 
         MenuInicial.Menu();
