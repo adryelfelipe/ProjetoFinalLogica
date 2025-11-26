f1221613690b31358167910a6976a038ac43b433
Adryel <adryxd003@gmail.com>
2025-11-25 21:43:36 -0300

    Feat: atualiza a classe main

--- src/main/java/Views/Sistema/Main.java
+++ src/main/java/Views/Sistema/Main.java
@@ -23,21 +23,7 @@ import Aplicacao.Ocorrencia.Mapper.OcorrenciaMapper;
 import Aplicacao.OrdemDeServico.Controller.OrdemDeServicoController;
 import Aplicacao.OrdemDeServico.Handler.OrdemDeServicoHandler;
 import Aplicacao.OrdemDeServico.Mapper.OrdemDeServicoMapper;
-import Dominio.Funcionario.Administrador.Administrador;
-import Dominio.Funcionario.Gerente.Gerente;
-import Dominio.Funcionario.Nucleo.Enumeracoes.Departamento;
-import Dominio.Funcionario.Nucleo.ObjetosDeValor.CPF;
-import Dominio.Funcionario.Nucleo.ObjetosDeValor.ListaDepartamentos;
-import Dominio.Funcionario.Nucleo.ObjetosDeValor.NomeFuncionario;
-import Dominio.Funcionario.Nucleo.ObjetosDeValor.Senha;
 import Dominio.Funcionario.Nucleo.Servicos.FuncionarioServico;
-import Dominio.Funcionario.Supervisor.ObjetosDeValor.MetaMensal;
-import Dominio.Funcionario.Supervisor.Supervisor;
-import Dominio.Funcionario.Tecnico.Enumeracoes.Especialidade;
-import Dominio.Funcionario.Tecnico.Tecnico;
-import Dominio.Maquina.Enumeracoes.StatusMaquina;
-import Dominio.Maquina.Maquina;
-import Dominio.Maquina.ObjetosDeValor.NomeMaquina;
 import Dominio.Maquina.Servicos.MaquinaServico;
 import Dominio.Ocorrencia.Servicos.OcorrenciaServico;
 import Dominio.OrdemDeServico.Servicos.OsServico;
@@ -45,15 +31,10 @@ import Infraestrutura.Persistencia.Implementacao.Funcionario.JDBC.FuncionarioRep
 import Infraestrutura.Persistencia.Implementacao.Funcionario.Mapper.FuncionarioJdbcMapper;
 import Infraestrutura.Persistencia.Implementacao.Maquina.JDBC.MaquinaJdbcRepositorio;
 import Infraestrutura.Persistencia.Implementacao.Maquina.Mapper.MaquinaJdbcMapper;
+import Infraestrutura.Persistencia.Implementacao.Ocorrencias.JDBC.OcorrenciasJDBCRepositorio;
 import Infraestrutura.Persistencia.Implementacao.OrdemDeServico.JDBC.OsJdbcRepositorio;
 import Infraestrutura.Persistencia.Implementacao.OrdemDeServico.Mapper.OsJdbcMapper;
-import Infraestrutura.Persistencia.Testes.Funcionario.FuncionarioSimulacaoJDBC;
-import Infraestrutura.Persistencia.Testes.Maquina.MaquinaSimulacaoJDBC;
-import Infraestrutura.Persistencia.Testes.Ocorrencia.OcSimulacaoJDBC;
-import Infraestrutura.Persistencia.Testes.OrdemDeServico.OsSimulacaoJDBC;
 import Views.Funcionario.Nucleo.MenuInicial;
-
-import java.util.Arrays;
 
 public class Main {
     // Dao para funcionario
@@ -71,6 +52,9 @@ public class Main {
     private static OsJdbcMapper osJdbcMapper = new OsJdbcMapper();
     private static OsJdbcRepositorio osDAO = new OsJdbcRepositorio(osJdbcMapper);
 
+    // Dao para ocorrência
+    // private static OcSimulacaoJDBC ocorrenciaDAO = new OcSimulacaoJDBC();
+    private static OcorrenciasJDBCRepositorio ocorrenciaDAO = new OcorrenciasJDBCRepositorio();
 
     // Aplicação de funcionário
     private static TipoFuncionarioServico tipoFuncionarioServico = new TipoFuncionarioServico();
@@ -97,15 +81,14 @@ public class Main {
     private static MaquinaHandler maquinaHandler = new MaquinaHandler(maquinaServico, maquinaMapper, autorizacaoServico, maquinaDAO);
 
     // Aplicação de ocorrência
-    private static OcSimulacaoJDBC ocorrenciaDao = new OcSimulacaoJDBC();
-    private static OcorrenciaServico ocorrenciaServico = new OcorrenciaServico(ocorrenciaDao);
+    private static OcorrenciaServico ocorrenciaServico = new OcorrenciaServico(ocorrenciaDAO);
     private static OcorrenciaMapper ocorrenciaMapper = new OcorrenciaMapper(maquinaDAO);
-    private static OcorrenciaHandler ocorrenciaHandler = new OcorrenciaHandler(ocorrenciaDao, ocorrenciaMapper, autorizacaoServico);
+    private static OcorrenciaHandler ocorrenciaHandler = new OcorrenciaHandler(ocorrenciaDAO, ocorrenciaMapper, autorizacaoServico);
 
     // Aplicação de Ordem de Serviço
     private static OsServico osServico = new OsServico(maquinaDAO, funcionarioDAO, osDAO);
     private static OrdemDeServicoMapper osMapper = new OrdemDeServicoMapper(funcionarioDAO, maquinaDAO);
-    private static OrdemDeServicoHandler osHandler = new OrdemDeServicoHandler(osDAO, autorizacaoServico, osServico, osMapper, maquinaDAO, funcionarioDAO, ocorrenciaMapper, ocorrenciaDao);
+    private static OrdemDeServicoHandler osHandler = new OrdemDeServicoHandler(osDAO, autorizacaoServico, osServico, osMapper, maquinaDAO, funcionarioDAO, ocorrenciaMapper, ocorrenciaDAO);
 
     // Controllers públicas
     public static GerenteController gerenteController = new GerenteController(gerenteHandler);
@@ -122,9 +105,9 @@ public class Main {
         // Testes com ArrayList  Adryel Sapelli
         //funcionarioDAO.funcionarioArrayList.add(new Administrador(998,new NomeFuncionario("joaoAdmA"),new CPF("12345678910"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.MECANICA))));
         //funcionarioDAO.funcionarioArrayList.add(new Administrador(999,new NomeFuncionario("joaoAdmB"),new CPF("12345678911"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.ELETRICA))));
-        // ID : 3 -> funcionarioDAO.salvar(new Gerente(new NomeFuncionario("joaoGerenteA"),new CPF(""), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.MECANICA))));
+        // ID : 3 -> funcionarioDAO.salvar(new Gerente(new NomeFuncionario("joaoGerenteA"),new CPF("12345678912"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.MECANICA))));
         // ID : 4 -> funcionarioDAO.salvar(new Gerente(new NomeFuncionario("joaoGerenteB"),new CPF("12345678913"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.ELETRICA))));
-        // ID : 5 -> funcionarioDAO.salvar(new Tecnico(new NomeFuncionario("joaoTecnicoA"),new CPF("12345678914"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.MECANICA)), Especialidade.SOLDADOR));
+        // ID : 5 -> funcionarioDAO.salvar(new Tecnico(new NomeFuncionario("joaoTecnicoA"),new CPF("13243546576"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.MECANICA)), Especialidade.SOLDADOR));
         // ID : 6 -> funcionarioDAO.salvar(new Tecnico(new NomeFuncionario("joaoTecnicoB"),new CPF("12345678915"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.ELETRICA)), Especialidade.SOLDADOR));
         // ID : 7 -> funcionarioDAO.salvar(new Supervisor(new NomeFuncionario("joaoSupervisorA"),new CPF("12345678916"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.MECANICA)), new MetaMensal(60000)));
         // ID : 8 -> funcionarioDAO.salvar(new Supervisor(new NomeFuncionario("joaoSupervisorB"),new CPF("12345678917"), new Senha("123456@Aa"), new ListaDepartamentos(Arrays.asList(Departamento.ELETRICA)), new MetaMensal(60000)));
